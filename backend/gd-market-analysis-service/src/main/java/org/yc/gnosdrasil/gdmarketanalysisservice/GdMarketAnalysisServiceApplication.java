8dd87fa7c9d60b239cfdaa457ecdeadc672a4728
Mushi <144794503+Mouche00@users.noreply.github.com>
2025-04-01 12:16:42 +0000

    feature: add initial market analysis service

---
+++ backend/gd-market-analysis-service/src/main/java/org/yc/gnosdrasil/gdmarketanalysisservice/GdMarketAnalysisServiceApplication.java
@@ -0,0 +1,13 @@
+package org.yc.gnosdrasil.gdmarketanalysisservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdMarketAnalysisServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdMarketAnalysisServiceApplication.class, args);
+    }
+
+}
---
+++ backend/gd-market-analysis-service/src/test/java/org/yc/gnosdrasil/gdmarketanalysisservice/GdMarketAnalysisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yc.gnosdrasil.gdmarketanalysisservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdMarketAnalysisServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
